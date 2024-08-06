dfe8db43fedf09569a26c48daea91849efaccdac
ausray1294 <ausray1294@gmail.com>
2024-08-06 09:17:56 -0600

    setup a mojority of expected file layout

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect } from 'react'
+import { LoginProvider } from './context/LoginContext'
+import { Header } from './components/Head/Header'
+import { GeneralInventory } from './components/Body/GeneralInventory'
+import { ManagerInventory } from './components/Body/ManagerInventory'
+
+
+function App() {
+  return (
+    
+
+  );
+}
+
+export default App;
